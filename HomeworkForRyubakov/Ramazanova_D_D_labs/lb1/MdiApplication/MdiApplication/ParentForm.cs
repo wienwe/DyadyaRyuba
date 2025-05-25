e11683d50a6fa1919940e97e50c741a24ae4aa38
Dilyara <144837886+wienwe@users.noreply.github.com>
2025-05-25 14:11:48 +0300

    Add files via upload

---
+++ HomeworkForRyubakov/Ramazanova_D_D_labs/lb1/MdiApplication/MdiApplication/ParentForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MdiApplication
+{
+    public partial class ParentForm: Form
+    {
+        private int openDocuments = 0;
+        public ParentForm()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void tileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Show();
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
