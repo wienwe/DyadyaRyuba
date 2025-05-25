37f63293eb0093bb98dcc684ba03c1bc8c9bc30d
Dilyara <144837886+wienwe@users.noreply.github.com>
2025-05-25 14:16:07 +0300

    Add files via upload

---
+++ HomeworkForRyubakov/Ramazanova_D_D_labs/lb2/WinQuestion/WinQuestion/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WinQuestion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnyes = new System.Windows.Forms.Button();
+            this.btnno = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnyes
+            // 
+            this.btnyes.Location = new System.Drawing.Point(12, 64);
+            this.btnyes.Name = "btnyes";
+            this.btnyes.Size = new System.Drawing.Size(75, 23);
+            this.btnyes.TabIndex = 0;
+            this.btnyes.Text = "Да";
+            this.btnyes.UseVisualStyleBackColor = true;
+            this.btnyes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnno
+            // 
+            this.btnno.Location = new System.Drawing.Point(128, 64);
+            this.btnno.Name = "btnno";
+            this.btnno.Size = new System.Drawing.Size(75, 23);
+            this.btnno.TabIndex = 1;
+            this.btnno.Text = "Нет";
+            this.btnno.UseVisualStyleBackColor = true;
+            this.btnno.Click += new System.EventHandler(this.button2_Click);
+            this.btnno.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnno_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Вы довольны своей зарплатой?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 149);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnno);
+            this.Controls.Add(this.btnyes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Насущный вопрос";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnyes;
+        private System.Windows.Forms.Button btnno;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
